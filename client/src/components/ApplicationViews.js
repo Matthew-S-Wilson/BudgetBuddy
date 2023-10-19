c55c0540f06ce784aa31059da52d37bba88ececd
Matthew-S-Wilson <matthew255@gmail.com>
2023-10-18 21:03:39 -0500

    Login and Registration Page work, direct to Homepage, still need to set the route for hompage to /Home

--- client/src/components/ApplicationViews.js
+++ client/src/components/ApplicationViews.js
@@ -1,11 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-import Bikes from "./bikes/Bikes";
 import { AuthorizedRoute } from "./auth/AuthorizedRoute";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
-import WorkOrderList from "./workorders/WorkOrderList.js";
-import CreateWorkOrder from "./workorders/CreateWorkOrder.js";
-import UserProfileList from "./userprofiles/UserProfileList.js";
+import { HomePage } from "./calculator/Homepage.js";
+
 
 export default function ApplicationViews({ loggedInUser, setLoggedInUser }) {
   return (
@@ -15,7 +13,7 @@ export default function ApplicationViews({ loggedInUser, setLoggedInUser }) {
           index
           element={
             <AuthorizedRoute loggedInUser={loggedInUser}>
-              <Bikes />
+              <HomePage />
             </AuthorizedRoute>
           }
         />
@@ -29,6 +27,7 @@ export default function ApplicationViews({ loggedInUser, setLoggedInUser }) {
           element={<Register setLoggedInUser={setLoggedInUser} />}
         />
       </Route>
+      
       <Route path="*" element={<p>Whoops, nothing here...</p>} />
     </Routes>
   );
